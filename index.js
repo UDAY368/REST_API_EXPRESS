0fb7480236e0310b34fadb73ac51908ea47ea1bf
K UDAY KUMAR <72476990+UDAY368@users.noreply.github.com>
2022-02-24 11:47:55 +0530

    This is my first REST API

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const router = require('./userRoutes');
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/api",router)
+
+app.get("/",(req,res)=>{
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}!`))
